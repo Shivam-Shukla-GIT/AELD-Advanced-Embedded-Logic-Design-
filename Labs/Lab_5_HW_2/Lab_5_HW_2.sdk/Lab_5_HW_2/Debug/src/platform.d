3281f692bf0695c51f82c361cce9a1dd06325b6f
Shivam-Shukla-GIT <shivam22478@iiitd.ac.in>
2025-12-27 23:10:32 +0530

    Add Lab_5_HW_2

---
+++ Labs/Lab_5_HW_2/Lab_5_HW_2.sdk/Lab_5_HW_2/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Lab_5_HW_2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
