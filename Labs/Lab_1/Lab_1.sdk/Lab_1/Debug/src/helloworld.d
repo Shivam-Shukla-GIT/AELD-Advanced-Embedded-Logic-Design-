368575f6f8ece679abf020b1bd26f673808004b0
Shivam-Shukla-GIT <shivam22478@iiitd.ac.in>
2025-12-27 22:38:14 +0530

    Add Lab_1

---
+++ Labs/Lab_1/Lab_1.sdk/Lab_1/Debug/src/helloworld.d
@@ -0,0 +1,18 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../Lab_1_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../Lab_1_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Lab_1_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Lab_1_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Lab_1_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../Lab_1_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Lab_1_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Lab_1_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
