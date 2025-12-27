60c298a0a80d535ee59407d762d6f59851aec3f5
Shivam-Shukla-GIT <shivam22478@iiitd.ac.in>
2025-12-27 22:39:42 +0530

    Add Lab_2_HW

---
+++ Labs/Lab_2_HW/Lab_2_HW.sdk/Lab_2_HW/Debug/src/helloworld.d
@@ -0,0 +1,27 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/platform.h ../src/platform_config.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_cache.h
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_cache.h:
---
+++ Labs/Lab_2_HW/Lab_2_HW.sdk/Lab_2_HW/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Lab_2_HW_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
