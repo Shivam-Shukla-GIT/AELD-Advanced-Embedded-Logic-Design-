f6c917b5210de368c8ea71ef6e9aa3f3719d6753
Shivam-Shukla-GIT <shivam22478@iiitd.ac.in>
2025-12-27 22:57:14 +0530

    Add Lab_5_HW

---
+++ Labs/Lab_5_HW/Lab_5_HW.cache/ip/2019.1/88a3782c460e7dc7/design_1_fir_compiler_0_0_stub.v
@@ -0,0 +1,31 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Fri Feb 14 15:57:17 2025
+// Host        : Shivam-Shukla running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ design_1_fir_compiler_0_0_stub.v
+// Design      : design_1_fir_compiler_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fir_compiler_v7_2_12,Vivado 2019.1" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(aresetn, aclk, s_axis_data_tvalid, 
+  s_axis_data_tready, s_axis_data_tlast, s_axis_data_tdata, m_axis_data_tvalid, 
+  m_axis_data_tready, m_axis_data_tlast, m_axis_data_tdata)
+/* synthesis syn_black_box black_box_pad_pin="aresetn,aclk,s_axis_data_tvalid,s_axis_data_tready,s_axis_data_tlast,s_axis_data_tdata[31:0],m_axis_data_tvalid,m_axis_data_tready,m_axis_data_tlast,m_axis_data_tdata[47:0]" */;
+  input aresetn;
+  input aclk;
+  input s_axis_data_tvalid;
+  output s_axis_data_tready;
+  input s_axis_data_tlast;
+  input [31:0]s_axis_data_tdata;
+  output m_axis_data_tvalid;
+  input m_axis_data_tready;
+  output m_axis_data_tlast;
+  output [47:0]m_axis_data_tdata;
+endmodule
---
+++ Labs/Lab_5_HW/Lab_5_HW.sdk/Lab_5_HW/Debug/src/helloworld.d
@@ -0,0 +1,66 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/platform.h ../src/platform_config.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Lab_5_HW_bsp/ps7_cortexa9_0/include/xparameters.h:
