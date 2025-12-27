582c470e2fa69af2772ca8d445d52e6e5015b60d
Shivam-Shukla-GIT <shivam22478@iiitd.ac.in>
2025-12-27 22:49:22 +0530

    Add Lab_5

---
+++ Labs/Lab_5/Lab_5.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_lab_5.elf_on_zed.tcl
@@ -0,0 +1,17 @@
+connect -url tcp:127.0.0.1:3121
+source D:/Sem_6/AELD/Labs/Lab_5/Lab_5.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zed 210248722900"} -index 0
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zed 210248722900" && level==0} -index 1
+fpga -file D:/Sem_6/AELD/Labs/Lab_5/Lab_5.sdk/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zed 210248722900"} -index 0
+loadhw -hw D:/Sem_6/AELD/Labs/Lab_5/Lab_5.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zed 210248722900"} -index 0
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zed 210248722900"} -index 0
+dow D:/Sem_6/AELD/Labs/Lab_5/Lab_5.sdk/Lab_5/Debug/Lab_5.elf
+configparams force-mem-access 0
+bpadd -addr &main
